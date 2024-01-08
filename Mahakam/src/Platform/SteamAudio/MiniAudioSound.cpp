7652787019539ba7177b8e41085d441b43cbcb51
KredeGC <krede2000@gmail.com>
2024-01-08 20:07:10 +0100

    Some initial design work on audio asset serialization

--- Mahakam/src/Platform/SteamAudio/MiniAudioSound.cpp
+++ Mahakam/src/Platform/SteamAudio/MiniAudioSound.cpp
@@ -13,7 +13,25 @@ namespace Mahakam
 
     MiniAudioSound::MiniAudioSound(const std::string& filepath, const SoundProps& props, MiniAudioContext* context)
         : m_Filepath(filepath), m_Props(props)
-	{ }
+	{
+        // TODO: Use stb_vorbis as internal format?
+        // TODO: Read file as binary (wav, mp3)
+
+        // Decode the file
+        //ma_decoder decoder;
+        //ma_decoder_init_memory(nullptr, 0, nullptr, &decoder);
+
+        // Read the length of the decoded file
+        //uint64_t length;
+        //ma_decoder_get_length_in_pcm_frames(&decoder, &length);
+
+        // Read the PCM frames back into a buffer
+        //void* frames;
+        //ma_result result = ma_decoder_read_pcm_frames(&decoder, frames, framesToRead, &framesRead);
+        //if (framesRead < framesToRead) {
+        //    // Reached the end.
+        //}
+    }
 
     void MiniAudioSound::SetProps(const SoundProps& props)
     {
